229cfd350331992f8c761113cd21858d5cd904b1
axelbriandip <axelbriandip.rg@gmail.com>
2023-01-01 21:29:36 -0300

    nav done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,6 +4,10 @@ const Nav = () => {
 
     return (
         <div className="nav">
+            <div className="logos">
+                <div className="logo-ol">Our League</div>
+                <div className="logo-cli">Liga Independiente</div>
+            </div>
             <ul>
                 <li><Link to={'/'}>Home</Link></li>
                 <li><Link to={'/listings'}>Listings</Link></li>
